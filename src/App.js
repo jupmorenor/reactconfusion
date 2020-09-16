d4595d68a0dd11bb6d395316f4cd59d38d1a1a58
Juanpa <jupmorenor@gmail.com>
2020-09-16 14:53:53 -0500

    adding state to Components, components part 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap'
 import Menu from './components/Menu'
 import './App.css';
+import { DISHES } from './data/dishes'
 
-function App() {
-  return (
-    <>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Confusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <header className="jumbotron">
-        <Menu />
-      </header>
-
-    </>
-  );
+class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      dishes: DISHES
+    }
+  }
+  render() {
+    return (
+      <>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Confusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <header className="jumbotron">
+          <Menu dishes={this.state.dishes} />
+        </header>
+  
+      </>
+    );
+  }
 }
 
 export default App;
