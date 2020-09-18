d3a48697d4bf9026f4de44b210b994ef996c6e0c
Juanpa <jupmorenor@gmail.com>
2020-09-18 17:15:30 -0500

    Data and render for Home Component: SPA part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,21 +2,32 @@ import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent'
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../data/dishes';
+import { COMMENTS } from '../data/comments';
+import { LEADERS } from '../data/leaders';
+import { PROMOTIONS } from '../data/promotions';
 
 class Main extends Component {
     constructor(props) {
         super();
         this.state = {
-            dishes: DISHES
+            dishes: DISHES,
+            comments: COMMENTS,
+            leaders: LEADERS,
+            promotions: PROMOTIONS
         }
     }
 
   render() {
     const HomePage = () => {
-      return <Home />
+      return <Home 
+        dish={this.state.dishes.filter((dish) => dish.featured )[0] } 
+        leader={this.state.leaders.filter((leader) => leader.featured )[0] } 
+        promotion={this.state.promotions.filter((promotion) => promotion.featured )[0] } 
+      />
     }
     
     return (
@@ -25,6 +36,7 @@ class Main extends Component {
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
