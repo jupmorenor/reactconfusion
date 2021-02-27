6d3fd4fc0ca68da886ae182030c147bbb1bc7b37
Juanpa <jupmorenor@gmail.com>
2021-02-26 19:44:23 -0500

    Redux reduced state

---
+++ src/redux/leaders.js
@@ -0,0 +1,9 @@
+import { LEADERS } from '../data/leaders';
+
+export const Leaders = (state = LEADERS, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
